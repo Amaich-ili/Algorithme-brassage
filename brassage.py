79267cacb91ff31ca5c792b73522d918f2ca593c
Iliass <iliamaich@gmail.com>
2022-08-18 12:11:20 -0400

    Création de la fonction sauvegarde

--- brassage.py
+++ brassage.py
@@ -1,5 +1,5 @@
 
-# Étape 1
+
 
 liste_cartes = ["A♥","2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","10♥","J♥","Q♥","K♥",
                 "A♦","2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","10♦","J♦","Q♦","K♦",
@@ -7,24 +7,23 @@ liste_cartes = ["A♥","2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","
                 "A♠","2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","10♠","J♠","Q♠","K♠"]
 #print(len(liste_cartes))
 
-# Étape 2
+
 def Affichage(liste_cartes):
     for i in range(0,len(liste_cartes),13):
         print(" ".join(liste_cartes[i: i+13]))
 
 
-from asyncore import read
 import random
 def Melange_Carte(liste_cartes):
-    liste_carte_melange = random.sample(liste_cartes,52)
+    liste_carte_melange = []
+    return liste_carte_melange == random.sample(liste_cartes,52)
     return(Affichage(liste_carte_melange))
 
 
-from asyncore import read
 def Sauvegarder():
-    with open("resultat.txt", "r") as fichier:
-        print(fichier.read())
-        #fichier.write(Melange_Carte())
+    fichier = open("cards.txt", "w")
+    fichier.write(str(Melange_Carte(liste_cartes)))
+    fichier.close()
 	
    
 menu = True
@@ -32,11 +31,13 @@ while menu:
     menu = int(input("Choisissez votre option : "))
     if menu == 1:
         Affichage(liste_cartes)
-    if menu == 2:
+    elif menu == 2:
         Melange_Carte(liste_cartes)
-    if menu == 3:
+    elif menu == 3:
         Sauvegarder()
+        print("\nFIN DE PROGRAMME")
         exit()
+    else:
+        menu == False
     print("1- Afficher l'état du jeu de carte \n2- Effectuer un brassage inter-coupé \n3- Sauvegarder l'état final dans un fichier .txt")
 
-print("\nFIN DE PROGRAMME")
