457c21a08f42a88c84fd4e2cdcedd87f15de00c7
Iliass <iliamaich@gmail.com>
2022-08-18 17:15:49 -0400

    Créer le menu

--- brassage.py
+++ brassage.py
@@ -1,17 +1,13 @@
 
-
-
 liste_cartes = ["A♥","2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","10♥","J♥","Q♥","K♥",
                 "A♦","2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","10♦","J♦","Q♦","K♦",
                 "A♣","2♣","♣","4♣","5♣","6♣","7♣","8♣","9♣","10♣","J♣","Q♣","K♣",
                 "A♠","2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","10♠","J♠","Q♠","K♠"]
 #print(len(liste_cartes))
 
-
 def Affichage(liste_cartes):
     for i in range(0,len(liste_cartes),13):
         print(" ".join(liste_cartes[i: i+13]))
-
 
 
 def Melange_Carte(liste_cartes):
@@ -41,7 +37,7 @@ def Sauvegarder(cartes):
     f.write(cartes_str)
     f.close()
 	
-   
+
 menu = True
 while menu:
     menu = int(input("Choisissez votre option : "))
